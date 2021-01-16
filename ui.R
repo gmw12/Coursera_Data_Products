15fb1767fbb7170c4a2f22e0379abb73500c7b3d
gmw12 <greg.waitt@duke.edu>
2021-01-16 15:34:33 -0500

    adde documentation

--- ui.R
+++ ui.R
@@ -3,7 +3,26 @@ library(shiny)
 shinyUI(fluidPage(
   titlePanel("Loan Amortization"),
   
-  sidebarLayout(
+  navlistPanel(widths=c(2,10), "Menu",
+  
+ tabPanel("Documentation",
+          
+         h2("Welcome to the Amortization App!"),
+         br(),
+         br(),
+         h3("This is a simple app to calculate a loan payment"),
+         br(),
+         h4("Step 1 - Click on 'Application' in the side panel"),
+         h4("Step 2 - Enter total loan amount in the first input box"),
+         h4("Step 3 - Enter down payment in the second input box"),
+         h4("Step 4 - Use slider to set the loan length in years"),
+         h4("Step 5 - Use slider to set the interest rate"),
+         h4("Step 6 - Click on 'Calculate' to find payment and see a graph of interest and principal over the life of the loan")
+ ), 
+    
+ tabPanel("Application",           
+ 
+ sidebarLayout(
     
     sidebarPanel(
       numericInput("loan", label = h3("Total Loan Amount"), value = 21000),
@@ -31,3 +50,5 @@ shinyUI(fluidPage(
   
   )
 ))
+)
+)
